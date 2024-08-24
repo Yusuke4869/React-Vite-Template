5d9bad6734d49404b2f4c729688de61e78b4e561
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-08-24 10:58:50 +0900

    fix: exports and imports

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import "~/styles/global.css";
 
-import Router from "./routes";
+import { Router } from "./routes";
 
 import type { FC } from "react";
 
-const App: FC = () => (
+export const App: FC = () => (
   <StrictMode>
     <Router />
   </StrictMode>
 );
-
-export default App;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,7 @@
 import type { FC } from "react";
 
-const Home: FC = () => (
+export const Home: FC = () => (
   <div>
     <h1>Hello World</h1>
   </div>
 );
-
-export { Home };
